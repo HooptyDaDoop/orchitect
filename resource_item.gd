35cdec5a2a27ba8569f110edb85f0e85fb71e9ba
Aron Kvaszta <kvaszta.aron.24@outlook.com>
2024-07-20 23:57:49 +0200

    Another commit

--- resource_item.gd
+++
@@ -1,9 +0,0 @@
-class_name ResourceItem extends Resource
-
-
-@export_category('Item Info')
-@export var item_name: String = ""
-@export_multiline var item_description: String = ""
-@export var item_texture: Texture2D = null
-
-@export var drop = null
